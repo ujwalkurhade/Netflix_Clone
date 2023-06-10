2a44f10d619ccc6d7712a8b87ed2170362023cf6
Ujwal <ujwalkurhade05@gmail.com>
2023-06-10 14:59:41 +0530

    ooo

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import './index.css'
-import Hero from './component/Hero'
-import Feture from './component/Feture'
-
+import React from "react";
+import "./index.css";
+import Hero from "./component/Hero";
+import Feture from "./component/Feture";
+import data from "./data.js";
 
 function App() {
   return (
     <>
-    <Hero/>
-    <Feture/>
+      <Hero />
+
+      <Feture />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/data.js
@@ -0,0 +1,11 @@
+import f1 from "./assets/Images/tv.png";
+
+export const data = [
+  {
+    id: 1,
+    h2: "shhdhsahdhasd",
+    p: "shhsadgasgdhgahfdfahdfa",
+    img: { f1 },
+  },
+  {},
+];
