21571edcc30ff0da0a0fd2780ed2b0f33e4bd006
Ujwal <ujwalkurhade05@gmail.com>
2023-06-10 14:32:50 +0530

    fatur created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './index.css'
 import Hero from './component/Hero'
+import Feture from './component/Feture'
 
 
 function App() {
   return (
     <>
     <Hero/>
+    <Feture/>
     </>
   )
 }
---
+++ src/component/Feture.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import f1 from "../assets/Images/tv.png";
+
+function Feture() {
+  return (
+    <div className="feture">
+      <h2> Enjoy on your TV </h2>
+      <p>
+        Watch on smart TVs, Playstation, Xbox, Chromecast,
+        Apple TV,Blu-ray player and more.
+      </p>
+      <img src={f1}></img>
+    </div>
+  );
+}
+
+export default Feture;
