77642bcdd2cfb0b2f2812fb94e0df8712e1fe471
Ujwal <ujwalkurhade05@gmail.com>
2023-06-11 11:36:25 +0530

    second day commit

--- src/component/Feture.jsx
+++ src/component/Feture.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import f1 from "../assets/Images/tv.png";
+// import data1 from "../data.js";
+import data from "../data.json";
 
 function Feture() {
+  const [data1, setdata1] = useState([]);
+
+  useEffect(() => {
+    setdata1(data.data);
+  }, []);
   return (
     <div className="feture">
-      <h2> Enjoy on your TV </h2>
-      <p>
-        Watch on smart TVs, Playstation, Xbox, Chromecast,
-        Apple TV,Blu-ray player and more.
-      </p>
-      <img src={f1}></img>
+      {data1.map((e) => {
+        return (
+          <>
+            <h2> {e.h2}</h2>
+            <p>{e.p}</p>
+            <img src={e.img}></img>
+          </>
+        );
+      })}
     </div>
   );
 }
