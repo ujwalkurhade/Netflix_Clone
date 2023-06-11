cfc20dd6250df593110cc23f43faaebeecc0c437
Ujwal <ujwalkurhade05@gmail.com>
2023-06-11 19:23:04 +0530

    convert Css File into Scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./index.css";
+import "./index.scss";
 import Hero from "./component/Hero";
 import Feture from "./component/Feture";
 import Qustionsection from "./component/Qustionsection";
