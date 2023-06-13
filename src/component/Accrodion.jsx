fa17febce38800557fae4593c0974b8bdcf52aac
Ujwal <ujwalkurhade05@gmail.com>
2023-06-13 10:18:53 +0530

    today commit

--- src/component/Accrodion.jsx
+++ src/component/Accrodion.jsx
@@ -4,20 +4,22 @@ function Accrodion({ title, desc }) {
   const [open, setOpen] = useState(false);
   const handlClick = () => setOpen(!open);
   return (
-    <div className="accordion">
-      <div
-        className=" acoridio_header"
-        onClick={handlClick}
-      >
-        <h2>{title}</h2>
-        {open ? <p>× </p> : <p>+</p>}
-      </div>
-
-      {open ? (
-        <div className="accordion_body">
-          <p>{desc}</p>
+    <div>
+      <div className="accordion">
+        <div
+          className=" acoridio_header"
+          onClick={handlClick}
+        >
+          <h2>{title}</h2>
+          {open ? <p>× </p> : <p>+</p>}
         </div>
-      ) : null}
+
+        {open ? (
+          <div className="accordion_body">
+            <p>{desc}</p>
+          </div>
+        ) : null}
+      </div>
     </div>
   );
 }
--- src/component/Qustionsection.jsx
+++ src/component/Qustionsection.jsx
@@ -3,21 +3,22 @@ import data from "../data.json";
 import SatartBtn from "./SatartBtn";
 import Accrodion from "./Accrodion";
 function Qustionsection() {
-  const [open, setOpen] = useState(false);
   const [data2, setdata2] = useState([]);
 
   useEffect(() => {
     setdata2(data.data2);
-    console.log(data2);
+    console.log(data.data2);
   }, []);
-  const handlClick = () => setOpen(!open);
 
   return (
     <div className="qustion">
       <h2>Frequently Asked Questions</h2>
       {data2.map((e, index) => {
-        console.log(e);
-        return <Accrodion desc={e.desc} title={e.title} />;
+        return (
+          <div key={index}>
+            <Accrodion desc={e.desc} title={e.title} />
+          </div>
+        );
       })}
       <div className="text">
         <p className="para">
